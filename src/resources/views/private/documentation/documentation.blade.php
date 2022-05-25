f3508fcdf5b9e527f7f2623a42d6e3386cdb5139
DCRDAW <dcarrionrodriguez.guadalupe@alumnado.fundacionloyola.net>
2022-05-25 19:32:26 +0200

    UPDATE:private_views

--- src/resources/views/private/documentation/documentation.blade.php
+++ src/resources/views/private/documentation/documentation.blade.php
@@ -1,4 +1,7 @@
 <x-app-layout>
+    <x-slot name="script">
+        {{ __('documentation') }}
+    </x-slot>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('documentation') }}
--- src/resources/views/private/inscription/inscription.blade.php
+++ src/resources/views/private/inscription/inscription.blade.php
@@ -1,4 +1,7 @@
 <x-app-layout>
+    <x-slot name="script">
+        {{ __('inscription') }}
+    </x-slot>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Inscription') }}
