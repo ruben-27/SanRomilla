f59ce8b9f642a320e87ac521bb670749827480f3
ruben-27 <rubentg10@gmail.com>
2022-06-03 00:21:27 +0200

    UPDATE: donations view finished

--- src/resources/views/private/donation/donation.blade.php
+++ src/resources/views/private/donation/donation.blade.php
@@ -8,17 +8,20 @@
         </h2>
     </x-slot>
     {{ csrf_field() }}
-    <div id="tabla" class="container" >
-                <div class="jumbotron jumbotron-fluid">
-                    <div class="mt-5 mb-5">
-                        <h1>Gestión donaciones</h1>
-                    </div>
-                </div>
-                <div><a href="/donation_showInsert"><button type="button" class="btn btn-primary mb-5" name="enviar" id="donation"><i class="bi bi-plus"></i>Nueva donación</button></a></div>
-                <div class="m-0">
-                    <livewire:donation-datatable/>
-                </div>
+    <div class="container mx-auto">
+        <div class="mt-5 md:mx-4">
+            <h1 class="text-3xl font-extrabold tracking-tight text-gray-700 text-center md:text-left sm:leading-none md:text-5xl">
+                <span class="inline md:block">Donaciones</span>
+            </h1>
+            <div class="flex justify-center md:justify-start">
+                <a href="/donation_showInsert" class="mt-7 flex bg-yellow-500 rounded-lg font-bold text-white px-4 py-3 transition duration-300 ease-in-out hover:bg-yellow-600 mr-6">
+                    <svg class="w-6 h-6" data-darkreader-inline-stroke="" fill="none" stroke="currentColor" style="--darkreader-inline-stroke: currentColor;" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+                    Añadir Categoría
+                </a>
             </div>
-            <div id="mostrarInsertar" class="container"></div>
-            <div id="cuadroTramitar" class="container"></div>
+        </div>
+        <livewire:donation-datatable/>
+    </div>
+    <div id="mostrarInsertar" class="container"></div>
+    <div id="cuadroTramitar" class="container"></div>
 </x-app-layout>
